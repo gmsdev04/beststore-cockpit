0afa6bcb29690b47b5bd13390d40b850b69e34fb
Guilherme Maciel <gmsdev04@icloud.com>
2020-12-22 22:08:59 -0300

    incluindo novas regras de validação campo NUMERO

--- src/components/tipos-de-dados/configuracao/ConfiguracaoTipoDeDadoNumero.vue
+++ src/components/tipos-de-dados/configuracao/ConfiguracaoTipoDeDadoNumero.vue
@@ -14,6 +14,8 @@
                     <v-col cols="12" md="2">
                         <v-text-field
                             v-model="tipoDeDadoNumero.valorMinimo.valor"
+                            :rules="[rules.minimoMenorQueMaximo(tipoDeDadoNumero.valorMinimo.valor,tipoDeDadoNumero.valorMaximo.valor)]"
+                            :max="tipoDeDadoNumero.valorMaximo.valor"
                             type="number" 
                             label="Valor mínimo"
                             @input="limparSeSomenteInteiro"
@@ -26,6 +28,8 @@
                     <v-col cols="12"  md="2">
                         <v-text-field 
                             v-model="tipoDeDadoNumero.valorMaximo.valor"
+                            :rules="[rules.maximoMaiorQueMinimo(tipoDeDadoNumero.valorMaximo.valor,tipoDeDadoNumero.valorMinimo.valor)]"
+                            :min="tipoDeDadoNumero.valorMinimo.valor"
                             type="number" 
                             label="Valor máximo"
                             @input="limparSeSomenteInteiro"
@@ -40,9 +44,8 @@
                             :min="tipoDeDadoNumero.valorMinimo.valor"
                             :max="tipoDeDadoNumero.valorMaximo.valor"
                             v-model="tipoDeDadoNumero.valorPadrao.valor"
-                            :rules="[   rulesValorPadrao.valorPadraoMenorIgualMaximo(tipoDeDadoNumero.valorPadrao.valor,tipoDeDadoNumero.valorMaximo.valor),
-                                        rulesValorPadrao.valorPadraoMaiorIgualMinimo(tipoDeDadoNumero.valorPadrao.valor,tipoDeDadoNumero.valorMinimo.valor)]
-                                        "
+                            :rules="[   rules.padraoMenorQueMaximo(tipoDeDadoNumero.valorPadrao.valor,tipoDeDadoNumero.valorMaximo.valor),
+                                        rules.padraoMaiorIgualMinimo(tipoDeDadoNumero.valorPadrao.valor,tipoDeDadoNumero.valorMinimo.valor)]"
                             type="number" 
                             label="Valor padrão"
                             @input="limparSeSomenteInteiro"
@@ -82,13 +85,22 @@ export default {
         return {
             tipoDeDadoNumero : JSON.parse(JSON.stringify(this.tipoDeDado)),
             somenteInteiro : false,
-            rulesValorPadrao: {
-                    valorPadraoMenorIgualMaximo(valorPadrao,valorMaximo) {
+            rules: {
+                    padraoMenorQueMaximo(valorPadrao,valorMaximo) {
                         return valorPadrao <= parseFloat(valorMaximo) || `Valor padrão maior que o máximo`;
                     },
-                    valorPadraoMaiorIgualMinimo(valorPadrao,valorMinimo) {
+                    padraoMaiorIgualMinimo(valorPadrao,valorMinimo) {
                         return valorPadrao >= parseFloat(valorMinimo) || `Valor padrão menor que o mínimo`;
                     },
+
+                    minimoMenorQueMaximo(valorMinimo, valorMaximo){
+                        return valorMinimo <= parseFloat(valorMaximo) || `Valor mínimo maior que o máximo`;
+                    },
+
+                    maximoMaiorQueMinimo(valorMaximo, valorMinimo){
+                        return valorMaximo >= parseFloat(valorMinimo) || `Valor máximo menor que o mínimo`;
+                    }
+
             }
         }
     },
